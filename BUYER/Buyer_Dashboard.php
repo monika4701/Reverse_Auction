746d75aa474c2666ce1477b8b96c436fb62c5994
MansiVerma18 <mansi.verma_cs18@gla.ac.in>
2021-04-27 15:05:16 +0530

    commit

--- BUYER/Buyer_Dashboard.php
+++ BUYER/Buyer_Dashboard.php
@@ -12,23 +12,52 @@ include('Buyer_Structure.php');
         <h1> <b>Welcome Back <i><?php echo $_SESSION['uname'];?></i></b></h1>
             <hr class="col-sm-5 mx-auto">
         </div>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="row row-cols-1 row-cols-md-3 g-4 my-3">
         <?php
           include('connection.php');
           if($_GET['C_Name']=='none'){
-            $query=mysqli_query($connection,"Select * from items");
-            while($row=mysqli_fetch_array($query)){?>
+            $query=mysqli_query($connection,"Select * from items order by name");
+            $c=0;
+            while($row=mysqli_fetch_array($query)){
+              $c=$c+1;
+              ?>
               
                 <div class="col">
                   <div class="card shadow h-100">
                     <img src="../img/<?php echo $row['img_fname']?>" height=200 class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title"><?php echo $row['name'];?></b></h5>
+                      <p class="card-text"><?php echo $row['C_Name'];?></b></p>
                       <p class="card-text"><?php echo $row['description'];?></b></p>
                     </div>
                     <div class="card-footer">                      
                       <div class="d-grid gap-2">
-                        <button type="button" class="btn btn-info">Bid</button>
+                      <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#i<?php echo$c?>">View</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="i<?php echo$c?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">View</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div class="modal-body">
+                              <div class="card-body">
+                              <div class="container-sm">
+                                <img src="../img/<?php echo $row['img_fname']?>" class="img-thumbnail" alt="...">
+                              </div>
+                                <h5 class="card-title"><b>Name: </b><?php echo $row['name'];?></h5>
+                                <p class="card-text"><b>Category: </b><?php echo $row['C_Name'];?></p>
+                                <p class="card-text"><b>Description: </b><?php echo $row['description'];?></p>
+                              </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-success">Bid</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
 
@@ -39,30 +68,63 @@ include('Buyer_Structure.php');
           }
            else{
           $cname=$_GET['C_Name'];
-          $query=mysqli_query($connection,"Select * from items where C_Name='$cname'");
+          $query=mysqli_query($connection,"Select * from items where C_Name='$cname' order by name ");
+          $w=0;
           if(mysqli_num_rows($query)>0){
-          while($row=mysqli_fetch_array($query)){?>
+          while($row=mysqli_fetch_array($query)){
+            $w=$w+1;
+            ?>
+
                           <div class="col">
                   <div class="card shadow h-100">
                     <img src="../img/<?php echo $row['img_fname']?>" height=200 class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title"><?php echo $row['name'];?></b></h5>
+                      <p class="card-text"><?php echo $row['C_Name'];?></b></p>
                       <p class="card-text"><?php echo $row['description'];?></b></p>
                     </div>
                     <div class="card-footer">                      
                       <div class="d-grid gap-2">
-                        <button type="button" class="btn btn-info">Bid</button>
+                        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#c<?php echo $w;?>">View</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="c<?php echo $w?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog  modal-dialog-centered modal-dialog-scrollable">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div class="modal-body">
+                                <div class="card-body">
+                                <div class="container-sm">
+                                  <img src="../img/<?php echo $row['img_fname']?>"  class="img-thumbnail" height=100 alt="...">
+                                </div>
+                                
+                                  <h5 class="card-title"><b><?php echo $row['name'];?></b></h5>
+                                  <p class="card-text"><b><?php echo $row['C_Name'];?></b></p>
+                                  <p class="card-text"><b><?php echo $row['description'];?></b></p>
+                                </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
 
                   </div>
                 </div>
+
+
                 
           <?php            
           }}
           else{?>
           </div>
-          <div class="card bg-danger my-5">
+          <div class="card bg-danger my-3">
             <div class="card-body ">
               <i><?php echo"No Products Available"?></i>
             </div>
